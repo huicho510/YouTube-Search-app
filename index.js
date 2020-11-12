5b04022510c64f590a2589bca96135bea8867b96
Luis Cervantes <luiscerv1229@gmail.com>
2020-11-12 14:16:07 -0800

    started Js/search bar animation/expands and closes

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const API_KEY = "AIzaSyA5EbDZkP35WPXPJGinjjrLTsmf0kfgkvU";
+
+// Searchbar handler
+$(function () {
+  let searchField = $("#query");
+  let icon = $("#search-btn");
+
+  // Focus Event Handler
+
+  $(searchField).on("focus", function () {
+    $(this).animate(
+      {
+        width: "100%",
+      },
+      400
+    );
+    $(icon).animate(
+      {
+        right: "10px",
+      },
+      400
+    );
+  });
+
+  // Blur Event Handler
+  $(searchField).on("blur", function () {
+    if (searchField.val() == "") {
+      $(searchField).animate(
+        {
+          width: "45%",
+        },
+        400,
+        function () {}
+      );
+      $(icon).animate({
+        right: "360px",
+      });
+    }
+  });
+});
